863c259ad8539b5c2b48cc6e7c98fb13887ebc99
Amimaro <amir.zahlan@gmail.com>
2018-04-18 17:27:40 -0300

    Create unescape quotes method :sparkles:

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fs = require('fs')
 module.exports = async function csvTemplate(params) {
   try {
     let template = await readCSV(params.template)
-    let result = await render(template, params.data)
+    let result = await render(unescapeQuotes(template), params.data)
     await writeCSV(params.output, result)
     return true
   } catch (err) {
@@ -24,5 +24,10 @@ let writeCSV = async function(path, data) {
 }
 
 let render = async function(template, data) {
+  console.log(template)
   return ejs.render(template, data)
+}
+
+let unescapeQuotes =  function(data) {
+  return data.replace(/(\'|\")+\</g, '<').replace(/\>(\'|\")+/g, '>').replace(/(\'\')/g, '\'').replace(/(\"\")/g, '\"')
 }

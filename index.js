a8abdbddc7ade4f3c64f6ca531394461d8c1e4b5
Amimaro <amir.zahlan@gmail.com>
2018-04-18 15:55:52 -0300

    Generate template :sparkles:

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 'use strict'
 
-module.exports = function csvTemplate (options) {
-  options = options || {}
+const ejs = require('ejs')
+const fs = require('fs')
 
-  return true
+module.exports = async function csvTemplate(params) {
+  try {
+    let template = await readCSV(params.template)
+    let result = await render(template, params.data)
+    await writeCSV(params.output, result)
+    return true
+  } catch (err) {
+    console.error(`An error occured: ${err}`)
+    return false
+  }
+}
+
+let readCSV = async function(path) {
+  return fs.readFileSync(path).toString('utf8')
+}
+
+let writeCSV = async function(path, data) {
+  return fs.writeFileSync(path, data)
+}
+
+let render = async function(template, data) {
+  return ejs.render(template, data)
 }

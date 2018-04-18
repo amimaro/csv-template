902e698b317149518107cfac240340e155650544
Amimaro <amir.zahlan@gmail.com>
2018-04-18 17:27:18 -0300

    Create test :rotating_light:

--- test.js
+++ test.js
@@ -1,8 +1,36 @@
 import test from 'ava'
 import csvTemplate from './'
 
-test('csv-template does something awesome', t => {
+test('csv-template example', t => {
   t.plan(1)
+  return csvTemplate({
+    template: './example.csv',
+    output: './output.csv',
+    data: {
+      name: 'John Doe',
+      age: 26,
+      email: 'johndoe@example.com',
+      birthdate: '01/01/1990',
+      projects: ['project1', 'project2', 'project3']
+    }
+  }).then((res) => {
+    t.true(res)
+  })
+})
 
-  t.true(csvTemplate())
+test('csv-template fail example', t => {
+  t.plan(1)
+  return csvTemplate({
+    template: './exampleFail.csv',
+    output: './output.csv',
+    data: {
+      name: 'John Doe',
+      age: 26,
+      email: 'johndoe@example.com',
+      birthdate: '01/01/1990',
+      projects: ['project1', 'project2', 'project3']
+    }
+  }).then((res) => {
+    t.false(res)
+  })
 })
